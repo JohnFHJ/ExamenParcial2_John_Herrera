e829ad86dfa32ab6ebb1209e8baeca5c371c6cb5
John <>
2017-01-31 20:30:16 -0600

    Service Listos

---
+++ src/main/java/ac/cr/una/backend/service/BookTypeService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.cr.una.backend.service;
+
+import ac.cr.una.backend.model.BookType;
+
+/**
+ *
+ * @author John
+ */
+public interface BookTypeService {
+    public boolean deleteAll();
+    public BookType save(BookType bookType);
+    public BookType findByNames(String name);
+}
---
+++ src/main/java/ac/cr/una/backend/service/BookTypeServiceImpl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.cr.una.backend.service;
+
+import ac.cr.una.backend.dao.BookTypeDAO;
+import ac.cr.una.backend.model.BookType;
+
+/**
+ *
+ * @author John
+ */
+public class BookTypeServiceImpl implements BookTypeService {
+
+    private BookTypeDAO bookTypeDAO;
+
+    public BookTypeServiceImpl() {
+    }
+
+    public BookTypeServiceImpl(BookTypeDAO bookTypeDAO) {
+        this.bookTypeDAO = bookTypeDAO;
+    }
+
+    @Override
+    public boolean deleteAll() {
+        return bookTypeDAO.deleteAll();
+    }
+
+    @Override
+    public BookType save(BookType bookType) {
+        return bookTypeDAO.save(bookType);
+    }
+
+    @Override
+    public BookType findByNames(String name) {
+        return bookTypeDAO.findByName(name);
+    }
+}
